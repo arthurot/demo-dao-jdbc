f1cc37faf9ce03a1d39c4ee18d6141aa85d27eb6
Arthur Tavares <arthurotavares@gmail.com>
2022-08-19 19:07:15 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,23 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
+		Locale.setDefault(Locale.US);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(1, "Arthur", "arthur@gmail.com", sdf.parse("17/03/1989"), 12.000, obj);
+		System.out.println(seller);
+		
 	}
 
 }
