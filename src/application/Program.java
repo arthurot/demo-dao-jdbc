0e0026e15e6afd91b4eec96aa8207ca95125ba21
Arthur Tavares <arthurotavares@gmail.com>
2022-08-20 16:46:27 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,10 +34,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TESTE 4: seller insert ===");
-		Seller newSeller = new Seller(null, "Michael Scott", "Mschott@gmail.com", new Date(), 4000.0, department);	
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
+//		System.out.println("\n=== TESTE 4: seller insert ===");
+//		Seller newSeller = new Seller(null, "Michael Scott", "Mschott@gmail.com", new Date(), 4000.0, department);	
+//		sellerDao.insert(newSeller);
+//		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TESTE 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Arthur Oliveira Tavares");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 
 }
