054feaa326e0676008a0b44439a9f8017f70bddf
Arthur Tavares <arthurotavares@gmail.com>
2022-08-19 21:48:20 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,21 @@
 package application;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
-		
-		Locale.setDefault(Locale.US);
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(1, "Arthur", "arthur@gmail.com", sdf.parse("17/03/1989"), 12.000, obj);
-		System.out.println(seller);
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
+		Seller seller = sellerDao.findById(3);
+
+		System.out.println(seller);
+
 	}
 
 }
